3167d380bf04f83262f5e5517bb71e5b6e12864e
SonerCoder <snrh4x0r@gmail.com>
2015-04-19 15:46:45 +0300

    number generator in c++

---
+++ genNum.cpp
@@ -0,0 +1,55 @@
+/*
+ * The program is guessing game, which works
+ * generating numbers btw 1 - 10, then
+ * Right number can be known in 5 choices.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int randNumGen( int x, int y );
+
+int main() {
+    
+    int n;
+    int generatedNum = randNumGen( 1, 10 );
+    
+    int count = 0;
+    
+    do {
+        cout << "Enter number =>  " ;
+        cin >> n;
+        count++;
+        
+        if ( n > generatedNum ) {
+            cout << "Right number is lower" << endl;
+        }
+        else if ( n < generatedNum ) {
+            cout << "Right number is higher" << endl;
+        }
+        else {
+            cout << "You get right number is " << n << endl;
+            break;
+        }
+    
+    }
+    while ( count < 5 );
+    
+    if ( count == 5 ) {
+        cout << "You didn't get right number is " << generatedNum << endl;
+    }
+    
+    
+    return 0;
+}
+
+int randNumGen( int x, int y ) {
+    
+    srand( (int) time( NULL ) );
+    
+    return 1 + rand() % y - x + 1;
+    
+}
